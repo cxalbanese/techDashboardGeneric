7c0be9ad666f1f21d271ff78ea0d0cda4960a91a
Chris Albanese <cxalbanese@gmail.com>
2022-03-02 21:43:55 -0500

    lds wire for Opportunity

---
+++ force-app/main/default/lwc/techOpptySummaryListView/techOpptySummaryListView.js
@@ -0,0 +1,49 @@
+import { api, LightningElement, track, wire } from 'lwc';
+import { refreshApex } from '@salesforce/apex';
+//import getOpptyList from '@salesforce/apex/techDashboard.getOpptyInfo';
+import { getListUi } from 'lightning/uiListApi';
+import OPPORTUNITY_OBJECT from '@salesforce/schema/Opportunity'; 
+import CLOSED_FIELD from '@salesforce/schema/Opportunity.IsClosed'
+import techId from '@salesforce/user/Id';
+
+export default class getOppties extends LightningElement {
+    @track opptyData = [];
+    @track errorData;
+    @track opptyDataWon = [];
+    @track errorDataWon;
+    @wire(getListUi,{
+        objectApiName : OPPORTUNITY_OBJECT, 
+        listViewApiName: 'My_Open_Opportunities',
+        sortBy: CLOSED_FIELD})
+    dataRecord({data, error}){
+       if(data){
+            this.opptyData = data.records.records;
+            console.log('data szie = ' + this.opptyData.length);
+       }
+       else if(error){
+           this.errorData = error;
+           this.opptyData = undefined;
+       }
+     }
+     @wire(getListUi,{
+        objectApiName : OPPORTUNITY_OBJECT, 
+        listViewApiName: 'My_Won_Opportunities',
+        sortBy: CLOSED_FIELD})
+    dataRecordWon({data, error}){
+       if(data){
+            this.opptyDataWon = data.records.records;
+            console.log('data szie = ' + this.opptyDataWon.length);
+       }
+       else if(error){
+           this.errorDataWon = error;
+           this.opptyDataWon = undefined;
+       }
+     }
+
+    get numOpenOppties() {
+        return this.opptyData.length;
+    }
+    get numWonOppties() {
+        return this.opptyDataWon.length;
+    }   
+}
