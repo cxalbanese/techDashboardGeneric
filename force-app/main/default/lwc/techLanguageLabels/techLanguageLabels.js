ed131a9e51fcfc9bd42690fa5b97bfcb868aee56
Chris Albanese <cxalbanese@gmail.com>
2022-03-07 07:55:05 -0500

    custom label for oppty header

--- force-app/main/default/lwc/techLanguageLabels/techLanguageLabels.js
+++ force-app/main/default/lwc/techLanguageLabels/techLanguageLabels.js
@@ -1,10 +1,12 @@
 import customlabelopenoppty from "@salesforce/label/c.techOpenOppty";
 import customlabelwonoppty from "@salesforce/label/c.techWonOppty"; 
+import customlabelopptyheader from "@salesforce/label/c.techOpptyHeader"; 
 import { LightningElement } from "lwc";
 
 export default class CustomLabelExampleLWC extends LightningElement {
   mylabels = {
     customlabelopenoppty,
-    customlabelwonoppty
+    customlabelwonoppty,
+    customlabelopptyheader
 };
 }
